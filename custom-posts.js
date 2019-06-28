0bdbf9744132e3663629e646b2d3e940c620a493
jessebettridge <jessebett92@gmail.com>
2019-06-28 16:12:11 -0600

    updated to expect content from input fields

--- custom-posts.js
+++ custom-posts.js
@@ -1,14 +1,32 @@
+
 document.getElementById("post-btn").addEventListener('click', send);
 
 function send() {
     let postTitle = document.getElementById("postTitle").value;
     let postContent = document.getElementById("postContent").value;
 
-    if (postTitle != '' || postContent != '') {
-        console.log(postTitle);
-        console.log(postContent);        
+    if (this.postTitle != '' && this.postContent != '') {        
+        const dataSend = new main(postTitle, postContent);
+        dataSend.datacheck();
+
     } else {
-        console.log('Please fill out Text Area')
-    }
+        console.log('Please fill out Text Area');
+
+    datacheck()
 }
+
+    class main {
+        constructor (postTitle, postContent) {
+            this.postTitle = postTitle;
+            this.postContent = postContent;
+        }
+
+        datacheck() {
+            postTitle = this.postTitle;
+            postContent = this.postContent;
+            return jsonify(postTitle, postContent);
+            }
+        }
+    }
+
 
